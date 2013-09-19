d603e1a4d8dd89b3ee583fa8a767b87244e75e79
gao.tang@oceanwing.com <gao.tang@oceanwing.com>
2013-09-19 21:53:02 +0800

    add

---
+++ common/src/main/java/com/ablaze/common/model/sale/Order.java
@@ -0,0 +1,17 @@
+package com.ablaze.common.model.sale;
+
+/**
+ * 
+* @ClassName: Order 
+* @Description: 订单的基本信息
+* @author ablaze
+* @date 2013-9-19 下午9:24:11
+ */
+public class Order {
+	
+	public static final String table = "sale_order";
+
+	public String marketplace;
+	public String order_id;
+//	public String 
+}
